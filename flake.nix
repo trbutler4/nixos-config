68f3ebe3f8928f15225373a63555a7a4e5887190
trbutler4 <trbiv@proton.me>
2025-06-14 10:09:57 -0500

    Add nvf integration with flake-based configuration
    - Add flake.nix with nvf, home-manager, and nixpkgs inputs
    - Create home.nix with nvf configuration including:
      - Catppuccin theme
      - LSP support for Nix, TypeScript, Rust, Python, Go
      - Treesitter syntax highlighting
      - Telescope fuzzy finder
      - Nvim-tree file explorer
      - Git integration with gitsigns
      - Lualine status bar
      - Nvim-cmp autocompletion
    - Update configuration.nix to remove deprecated options
    - Create update-flake.sh script for flake-based rebuilds
    - Copy hardware-configuration.nix to repository

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "NixOS configuration with nvf integration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, nvf, ... }@inputs: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.default
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.extraSpecialArgs = { inherit inputs; };
+            home-manager.users.trbiv = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,142 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    inputs.nvf.homeManagerModules.default
+  ];
+
+  # Basic home manager config
+  home.username = "trbiv";
+  home.homeDirectory = "/home/trbiv";
+  home.stateVersion = "25.05";
+
+  # Packages
+  home.packages = with pkgs; [
+    atool
+    httpie
+  ];
+
+  # Firefox
+  programs.firefox.enable = true;
+
+  # Zsh configuration
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true; 
+    syntaxHighlighting.enable = true;
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"        
+      ];
+      theme = "lambda";
+    };
+  };
+
+  # Fzf
+  programs.fzf = {
+    enable = true; 
+    enableZshIntegration = true;
+  };
+
+  # Basic nvf configuration
+  programs.nvf = {
+    enable = true;
+    settings = {
+      vim = {
+        viAlias = false;
+        vimAlias = true;
+        
+        # Basic options
+        lineNumberMode = "number";
+        preventJunkFiles = true;
+        
+        # Theme
+        theme = {
+          enable = true;
+          name = "catppuccin";
+          style = "mocha";
+        };
+
+        # Treesitter for syntax highlighting
+        treesitter = {
+          enable = true;
+          fold = true;
+        };
+
+        # LSP Configuration
+        lsp = {
+          enable = true;
+          formatOnSave = true;
+        };
+
+        # Language support
+        languages = {
+          nix.enable = true;
+          
+          ts = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          
+          rust = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          
+          python = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+          
+          go = {
+            enable = true;
+            lsp.enable = true;
+            treesitter.enable = true;
+          };
+        };
+
+        # Autocompletion
+        autocomplete = {
+          nvim-cmp.enable = true;
+        };
+
+        # File tree
+        filetree = {
+          nvimTree = {
+            enable = true;
+            mappings = {
+              toggle = "<leader>e";
+            };
+          };
+        };
+
+        # Telescope for fuzzy finding
+        telescope = {
+          enable = true;
+        };
+
+        # Git integration
+        git = {
+          enable = true;
+          gitsigns.enable = true;
+        };
+
+        # Status line
+        statusline = {
+          lualine = {
+            enable = true;
+            theme = "catppuccin";
+          };
+        };
+
+        # Leader key
+        globals.mapleader = " ";
+      };
+    };
+  };
+}
