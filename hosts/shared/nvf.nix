9ae9f126f928712599792e1d232f7f4d0759da50
trbutler4 <trbiv@proton.me>
2025-06-15 20:25:31 -0500

    remove keymaps, make which key window look like helix

--- hosts/shared/nvf.nix
+++ hosts/shared/nvf.nix
@@ -76,78 +76,10 @@
         binds = {
           whichKey = {
             enable = true;
+            setupOpts.preset = "helix";
           };
         };
 
-        keymaps = [
-          # LSP keybindings
-          {
-            mode = "n";
-            key = "gd";
-            action = "<cmd>lua vim.lsp.buf.definition()<CR>";
-            desc = "Go to definition";
-          }
-          {
-            mode = "n";
-            key = "gD";
-            action = "<cmd>lua vim.lsp.buf.declaration()<CR>";
-            desc = "Go to declaration";
-          }
-          {
-            mode = "n";
-            key = "gr";
-            action = "<cmd>lua vim.lsp.buf.references()<CR>";
-            desc = "Go to references";
-          }
-          {
-            mode = "n";
-            key = "gi";
-            action = "<cmd>lua vim.lsp.buf.implementation()<CR>";
-            desc = "Go to implementation";
-          }
-          {
-            mode = "n";
-            key = "K";
-            action = "<cmd>lua vim.lsp.buf.hover()<CR>";
-            desc = "Show hover information";
-          }
-          {
-            mode = "n";
-            key = "<C-k>";
-            action = "<cmd>lua vim.lsp.buf.signature_help()<CR>";
-            desc = "Show signature help";
-          }
-          {
-            mode = "n";
-            key = "<leader>rn";
-            action = "<cmd>lua vim.lsp.buf.rename()<CR>";
-            desc = "Rename symbol";
-          }
-          {
-            mode = "n";
-            key = "<leader>ca";
-            action = "<cmd>lua vim.lsp.buf.code_action()<CR>";
-            desc = "Code actions";
-          }
-          {
-            mode = "n";
-            key = "[d";
-            action = "<cmd>lua vim.diagnostic.goto_prev()<CR>";
-            desc = "Go to previous diagnostic";
-          }
-          {
-            mode = "n";
-            key = "]d";
-            action = "<cmd>lua vim.diagnostic.goto_next()<CR>";
-            desc = "Go to next diagnostic";
-          }
-          {
-            mode = "n";
-            key = "<leader>d";
-            action = "<cmd>lua vim.diagnostic.open_float()<CR>";
-            desc = "Show diagnostic in float";
-          }
-        ];
       };
     };
   };
