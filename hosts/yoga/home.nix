ba9d921b35ea9dda4cc3d0342a609da18e05f382
trbutler4 <trbiv@proton.me>
2025-06-22 19:02:28 -0500

    remove dconf setting because i dont think they were doing anything

--- hosts/yoga/home.nix
+++ hosts/yoga/home.nix
@@ -110,24 +110,4 @@
     enableZshIntegration = true;
   };
 
-  dconf = {
-    enable = true;
-    settings = {
-      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      "org/gnome/shell" = {
-        disable-user-extensions = false; # enables user extensions
-        enabled-extensions = [
-          # Put UUIDs of extensions that you want to enable here.
-          # If the extension you want to enable is packaged in nixpkgs,
-          # you can easily get its UUID by accessing its extensionUuid
-          # field (look at the following example).
-          pkgs.gnomeExtensions.tiling-shell.extensionUuid
-          pkgs.gnomeExtensions.hide-top-bar.extensionUuid
-          pkgs.gnomeExtensions.appindicator.extensionUuid
-          pkgs.gnomeExtensions.desktop-clock.extensionUuid
-        ];
-      };
-    };
-  };
-
 }
