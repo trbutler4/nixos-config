6a22a2db2b49ec024d9d83c786eeb3369cd20ff0
trbutler4 <trbiv@proton.me>
2025-07-24 21:54:00 -0500

    switch to master layout

--- hosts/shared/base-hyprland.nix
+++ hosts/shared/base-hyprland.nix
@@ -97,7 +97,7 @@
         border_size = 2;
         "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         "col.inactive_border" = "rgba(595959aa)";
-        layout = "dwindle";
+        layout = "master";
         allow_tearing = false;
       };
       
