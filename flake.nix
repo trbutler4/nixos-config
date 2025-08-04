fab4ec5e0902fb51a3ff2d322a78bede61d6c433
Thomas Butler <trbiv@proton.me>
2025-08-03 20:34:51 -0500

    nixpkgs cosmic

--- flake.nix
+++ flake.nix
@@ -2,9 +2,8 @@
   description = "NixOS configuration for multiple hosts";
 
   inputs = {
-    nixpkgs.follows = "nixos-cosmic/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,7 +19,6 @@
     {
       nixpkgs,
       nixpkgs-stable,
-      nixos-cosmic,
       home-manager,
       nvf,
       ...
@@ -44,13 +42,6 @@
         yoga = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            nixos-cosmic.nixosModules.default
             ./hosts/yoga/configuration.nix
             home-manager.nixosModules.default
             {
@@ -73,29 +64,6 @@
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = { inherit inputs; };
               home-manager.users.trbiv = import ./hosts/desktop/home.nix;
-              home-manager.users.guest = import ./hosts/desktop/guest-home.nix;
-            }
-          ];
-        };
-
-        # Configuration for desktop with COSMIC
-        desktop-cosmic = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            nixos-cosmic.nixosModules.default
-            ./hosts/desktop-cosmic/configuration.nix
-            home-manager.nixosModules.default
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.users.trbiv = import ./hosts/desktop-cosmic/home.nix;
               home-manager.users.guest = import ./hosts/desktop/guest-home.nix;
             }
           ];
